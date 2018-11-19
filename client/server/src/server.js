8c55ea910efa18c24ec84c09532d6284ab473fa8
Matt Roberts <code@rbrts.uk>
2018-11-19 12:02:18 +0000

    feat: Bump to Cicero 0.9.7. Better multi-file support
    Signed-off-by: Matt Roberts <code@rbrts.uk>

--- client/server/src/server.js
+++ client/server/src/server.js
@@ -13,14 +13,16 @@ const Ergo = require("@accordproject/ergo-compiler/lib/ergo");
 const CiceroModelManager = require("@accordproject/cicero-core/lib/ciceromodelmanager");
 const glob_1 = require("glob");
 const fs = require("fs");
+const path = require("path");
 const composer_concerto_1 = require("composer-concerto");
 // Creates the LSP connection
 let connection = vscode_languageserver_1.createConnection(vscode_languageserver_1.ProposedFeatures.all);
 // Create a manager for open text documents
 let documents = new vscode_languageserver_1.TextDocuments();
+// Cache the modelManager instances for each document
+let modelManagers = {};
 // The workspace folder this server is operating on
 let workspaceFolder;
-let modelManager = new CiceroModelManager();
 documents.onDidOpen((event) => {
     connection.console.log(`[Server(${process.pid}) ${workspaceFolder}] Document opened: ${event.document.uri}`);
 });
@@ -46,30 +48,42 @@ documents.onDidChangeContent((_) => __awaiter(this, void 0, void 0, function* ()
 // This function is not currently triggered by changes to model files, only ergo files
 function validateTextDocument(textDocument) {
     return __awaiter(this, void 0, void 0, function* () {
+        if (!modelManagers[textDocument.uri]) {
+            modelManagers[textDocument.uri] = new CiceroModelManager();
+        }
         let diagnostics = [];
         try {
-            // Find all cto files in ../ relative to this file
+            // Find all cto files in ./ relative to this file or in the parent director
+            // if this is a Cicero template.
             const folder = textDocument.uri.match(/^file:\/\/(.*\/)(.*)/)[1];
             const modelFilesContents = [];
             let newModels = false;
-            const modelFiles = glob_1.glob.sync(folder + '../**/*.cto');
+            const parentDir = path.resolve(`${folder}../`);
+            const modelFiles = glob_1.glob.sync(`{${folder},${parentDir}/models/}**/*.cto`);
             for (const file of modelFiles) {
                 const contents = fs.readFileSync(file, 'utf8');
-                const modelFile = new composer_concerto_1.ModelFile(modelManager, contents, file);
-                if (!modelManager.getModelFile(modelFile.getNamespace())) {
+                const modelFile = new composer_concerto_1.ModelFile(modelManagers[textDocument.uri], contents, file);
+                if (!modelManagers[textDocument.uri].getModelFile(modelFile.getNamespace())) {
                     // only add if not existing
-                    modelManager.addModelFile(contents, file, true);
+                    modelManagers[textDocument.uri].addModelFile(contents, file, true);
                     newModels = true;
                 }
             }
             // Only pull external models if a new file was added to the model manager
             if (newModels) {
-                yield modelManager.updateExternalModels();
+                yield modelManagers[textDocument.uri].updateExternalModels();
             }
-            modelManager.getModelFiles().map((f) => {
+            modelManagers[textDocument.uri].getModelFiles().map((f) => {
                 modelFilesContents.push({ name: '(CTO Buffer)', content: f.getDefinitions() });
             });
-            const compiled = yield Ergo.compileToJavaScript([{ name: '(Ergo Buffer)', content: textDocument.getText() }], modelManager.getModels(), 'cicero', true);
+            // Find all ergo files in ./ relative to this file
+            const ergoFilesContents = [{ name: '(Ergo Buffer)', content: textDocument.getText() }];
+            const ergoFiles = glob_1.glob.sync(`{${folder},${parentDir}/models/}**/*.ergo`);
+            for (const file of ergoFiles) {
+                const contents = fs.readFileSync(file, 'utf8');
+                ergoFilesContents.push({ name: file, content: contents });
+            }
+            const compiled = yield Ergo.compileToJavaScript(ergoFilesContents, modelManagers[textDocument.uri].getModels(), 'cicero', true);
             if (compiled.error) {
                 if (compiled.error.kind === 'CompilationError' || compiled.error.kind === 'TypeError') {
                     const range = {
